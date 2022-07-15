3133a56c8985bdac34a127ebec40f2ae5881ee6d
agustinlopez0 <70503126+agustinlopez0@users.noreply.github.com>
2022-07-15 00:10:58 -0300

    get entrenamiento listo

--- API/get_entrenamiento.php
+++ API/get_entrenamiento.php
@@ -2,7 +2,10 @@
 include 'headers.php';
 
 $mysqli = new mysqli($host, $user, $pass, $base);
-$res = $mysqli->query("select * from entrenamiento");
+$res = $mysqli->query("SELECT N.id, N.id_ejercicio, N.peso, N.dia, N.fecha, N.mes, N.anio, N.id_ejercicio, J.nombre_ejercicio, J.id_musculo, M.nombre_musculo
+FROM entrenamiento N
+JOIN ejercicios J ON J.id = N.id_ejercicio
+JOIN musculos M ON M.id = J.id_musculo");
 
 
 $rows = [];
