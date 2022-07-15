398b2e280045a41fff0dc1a6dbee6f8799a0b021
agustinlopez0 <70503126+agustinlopez0@users.noreply.github.com>
2022-07-15 13:13:48 -0300

    first commit router

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,30 @@
 <template>
-  <h1>Gym Tracker</h1>
-  <EjerciciosInput />
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script>
-
-import EjerciciosInput from './components/EjerciciosInput.vue'
-
-export default {
-  name: 'App',
-  components: {
-    EjerciciosInput
-  }
-}
-</script>
-
 <style>
-  *{
-    padding: 0;
-    margin: 0
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-  h1{
-    text-align: center;
-    padding: 20px 0 40px;
-  }
+nav {
+  padding: 30px;
+}
 
-  #app {
-    margin: none;
-    background-color: rgb(250, 250, 250);
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
